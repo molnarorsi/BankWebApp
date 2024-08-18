3f5df93a9cc429b85ba92767b8387ab6c9c2a4a4
Molnar Orsolya Izabella <molnar.orsolya@student.ms.sapientia.ro>
2024-08-18 13:23:52 -0700

    animated counter and totalbalancebox components

--- bankwebapp/app/(root)/page.tsx
+++ bankwebapp/app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
     const loggedInUser = {firstName: 'Buebu'};
@@ -13,7 +14,11 @@ const Home = () => {
                     title="Welcome to your Bank,"
                     user={loggedInUser?.firstName || 'Guest'}
                     subtext="Your money is safe with us"
-                />
+                /> 
+                <TotalBalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.35} />
             </header>
         </div>
        </section>
