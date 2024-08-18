e30234a05a24425a4ff92fc6d8bd39e289dd77a2
Molnar Orsolya Izabella <molnar.orsolya@student.ms.sapientia.ro>
2024-08-18 12:50:21 -0700

    headerbox

--- bankwebapp/app/(root)/page.tsx
+++ bankwebapp/app/(root)/page.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import HeaderBox from '@/components/HeaderBox';
 
 const Home = () => {
+    const loggedInUser = {firstName: 'Buebu'};
+
     return (
-        <div>
-        Home
+       <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox
+                    type="greeting"
+                    title="Welcome to your Bank,"
+                    user={loggedInUser?.firstName || 'Guest'}
+                    subtext="Your money is safe with us"
+                />
+            </header>
         </div>
+       </section>
     );
 }
 
