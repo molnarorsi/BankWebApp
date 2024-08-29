b0507714733f08c5b73d726b531a701ce0c64293
Molnar Orsolya Izabella <molnar.orsolya@student.ms.sapientia.ro>
2024-08-29 14:47:43 -0700

    signup with appwrite

--- bankwebapp/components/AuthForm.tsx
+++ bankwebapp/components/AuthForm.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
-import { set, z } from "zod"
+import {  z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
@@ -13,10 +13,14 @@ import {
 import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
+import {useRouter} from 'next/navigation';
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
 
 const AuthForm = ({type}: {type: string}) => {
+  const router = useRouter();
     const [user, setUser] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+    
 
     const formSchema = authFormSchema(type);
 
@@ -30,10 +34,31 @@ const AuthForm = ({type}: {type: string}) => {
   })
  
   // 2. Define a submit handler.
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
       setIsLoading(true)
-      console.log(values)
-      setIsLoading(false)
+      try {
+        //sign up with appwrite & create plaid link token
+        if(type === 'sign-up') {
+          const newUser = await signUp(data);
+          setUser(newUser);
+        }
+        if(type === 'sign-in') {
+          // const response = await signIn({
+          //   email: data.email,
+          //   password: data.password,
+          // });
+
+          // if(response) {
+          //   router.push('/');
+          // }
+
+        }
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setIsLoading(false)
+      }
+      
   }
   
   return (
@@ -75,6 +100,7 @@ const AuthForm = ({type}: {type: string}) => {
                           <CustomInput control={form.control} name='lastName' label="Last Name" placeholder='Enter your last name'/>
                         </div>
                           <CustomInput control={form.control} name='address1' label="Address" placeholder='Enter your specific address'/>
+                          <CustomInput control={form.control} name='city' label="City" placeholder='Enter your city'/>
                         <div className='flex gap-4'>
                           <CustomInput control={form.control} name='state' label="State" placeholder='Example: NY'/>
                           <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Example: 1101'/>
